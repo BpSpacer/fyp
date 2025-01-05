af29efb905e1fa0f84bce7335ea0a962e4f4c717
Muhammad Ahsan Kamal <ahsankamal9954@gmail.com>
2025-01-05 19:07:56 +0500

    Finish

--- src/app/storefront/bag/page.tsx
+++ src/app/storefront/bag/page.tsx
@@ -24,7 +24,6 @@ export default async function BagRoute() {
 
   const cart: Cart | null = await redis.get(`cart-${user.id}`);
 
-  let totalPrice = 0;
 
   cart?.items.forEach((item) => {
     totalPrice += item.price * item.quantity;
