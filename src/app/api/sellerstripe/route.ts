7f719d2e4e48870bde8da6672b62c9ee2409826a
Muhammad Ahsan Kamal <ahsankamal9954@gmail.com>
2025-01-09 15:47:23 +0500

    set

--- src/app/api/sellerstripe/route.ts
+++ src/app/api/sellerstripe/route.ts
@@ -1,6 +1,5 @@
 import prisma from "@/app/lib/sellerdb";
 import { stripe } from "@/app/lib/stripe";
-
 import { headers } from "next/headers";
 
 export async function POST(req: Request) {
@@ -16,32 +15,34 @@ export async function POST(req: Request) {
       signature,
       process.env.STRIPE_CONNECT_WEBHOOK_SECRET as string
     );
-  } catch (error: unknown) {
+  } catch {
+    // If an error occurs, log it for debugging and return an appropriate response
+    console.error("Error verifying Stripe webhook signature");
     return new Response("webhook error", { status: 400 });
   }
 
-  // switch (event.type) {
-  //   case "account.updated": {
-  //     const account = event.data.object;
+  switch (event.type) {
+    case "account.updated": {
+      const account = event.data.object as { id: string; capabilities?: { transfers: string } };
 
-  //     const data = await prisma.user.update({
-  //       where: {
-  //         connectedAccountId: account.id,
-  //       },
-  //       data: {
-  //         stripeConnectedLinked:
-  //           account.capabilities?.transfers === "pending" ||
-  //           account.capabilities?.transfers === "inactive"
-  //             ? false
-  //             : true,
-  //       },
-  //     });
-  //     break;
-  //   }
-  //   default: {
-  //     console.log("unhandled event");
-  //   }
-  // }
+      await prisma.user.update({
+        where: {
+          connectedAccountId: account.id,
+        },
+        data: {
+          stripeConnectedLinked:
+            account.capabilities?.transfers === "pending" ||
+            account.capabilities?.transfers === "inactive"
+              ? false
+              : true,
+        },
+      });
+      break;
+    }
+    default: {
+      console.log("Unhandled event type:", event.type);
+    }
+  }
 
   return new Response(null, { status: 200 });
 }
